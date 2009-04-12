caf7b2af2f9baf5f603d7e0525aa690063269f6c
Jacqui Maher <jacqui@brighter.net>
2009-04-12 18:29:48 -0400

    Replaced create_sample with FactoryGirl

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -10,31 +10,6 @@ describe Patient do
     Patient.send(:class_variable_set, :@@date_started_art, Hash.new)
   end
 
-  sample({
-    :patient_id => 1,
-    :gender => '',
-    :race => '',
-    :birthdate => Time.now.to_date,
-    :birthdate_estimated => false,
-    :birthplace => '',
-    :citizenship => '',
-    :mothers_name => '',
-    :civil_status => 1,
-    :dead => 1,
-    :death_date => Time.now,
-    :cause_of_death => '',
-    :health_district => '',
-    :health_center => 1,
-    :creator => 1,
-    :date_created => Time.now,
-    :changed_by => 1,
-    :date_changed => Time.now,
-    :voided => false,
-    :voided_by => 1,
-    :date_voided => Time.now,
-    :void_reason => '',
-  })
-
   it "should find concept by concept_id" do
     patient(:andreas).observations.find_by_concept_id(concept(:height).id).first.encounter.name.should == "Height/Weight"
   end
@@ -783,7 +758,7 @@ EOF
   end
 
   it "should be valid" do
-    patient = create_sample(Patient)
+    patient = Factory(:patient)
     patient.should be_valid
   end
 
